1040ecbb50369d5a8d884e20d7e8b3a2d28fda93
Xining Du <meiyisi@gmail.com>
2013-05-16 13:40:00 -0400

    If finish and start points are too close, do nothing.

--- fraction.cpp
+++ fraction.cpp
@@ -24,12 +24,13 @@ void specialKeys();
 
 using namespace std;
 
+const double delta = 0.001;
 
 struct Position
 {
-    Position() : x(0), y(0) {}
-    double x;
-    double y;
+	Position() : x(0), y(0) {}
+	double x;
+	double y;
 };
 
 Position start;
@@ -47,7 +48,7 @@ double yminOld = -3.0, ymaxOld = 3.0;
 double xminSelect = -3.0, xmaxSelect = 3.0;
 double yminSelect = -3.0, ymaxSelect = 3.0;
 
-const int MAX_ITER_NUM = 1280;
+const int MAX_ITER_NUM = 256;
 
 
 
@@ -65,49 +66,49 @@ GLfloat palette[MAX_ITER_NUM][3];
 // ----------------------------------------------------------
 void display(){
 
- glMatrixMode(GL_PROJECTION);
- glLoadIdentity();
- glOrtho(0, XSize, YSize, 0, 0, 1);
- glDisable(GL_DEPTH_TEST);
- glMatrixMode(GL_MODELVIEW);
- glLoadIdentity();
- glTranslatef(0.375, 0.375, 0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, XSize, YSize, 0, 0, 1);
+	glDisable(GL_DEPTH_TEST);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(0.375, 0.375, 0);
 
- glClearColor(.3, .3, .3, 0);
- glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(.3, .3, .3, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
 
- double x0, y0;
- double x = 0;
- double y = 0;
- double xtemp;
- int iterNum = 0;
- GLfloat* color;
- color = (GLfloat *)malloc(3*sizeof(GLfloat));
+	double x0, y0;
+	double x = 0;
+	double y = 0;
+	double xtemp;
+	int iterNum = 0;
+	GLfloat* color;
+	color = (GLfloat *)malloc(3*sizeof(GLfloat));
 
- glBegin(GL_POINTS);
- for(int i=0; i<XSize; i++)
-	 for (int j=0; j<YSize; j++)
-	 {
-		 x0 = xmin + (double)i/(double)XSize*(xmax - xmin);
-		 y0 = ymin + (double)j/(double)YSize*(ymax - ymin);
-		 // draw a line
-		x = 0;
-		y = 0;
-		iterNum = 0;
+	glBegin(GL_POINTS);
+	for(int i=0; i<XSize; i++)
+		for (int j=0; j<YSize; j++)
+		{
+			x0 = xmin + (double)i/(double)XSize*(xmax - xmin);
+			y0 = ymin + (double)j/(double)YSize*(ymax - ymin);
+			// draw a line
+			x = 0;
+			y = 0;
+			iterNum = 0;
 
-		  while ( x*x + y*y < 4  &  iterNum < MAX_ITER_NUM )
-		  {
-			xtemp = x*x - y*y + x0;
-			y = 2*x*y + y0;
-			x = xtemp;
-			iterNum ++;
-		  }
+			while ( x*x + y*y < 4  &  iterNum < MAX_ITER_NUM )
+			{
+				xtemp = x*x - y*y + x0;
+				y = 2*x*y + y0;
+				x = xtemp;
+				iterNum ++;
+			}
 
-		color = palette[iterNum];
-		glColor3fv(color);
-		glVertex2f(i, j);
+			color = palette[iterNum];
+			glColor3fv(color);
+			glVertex2f(i, j);
 
-	 }
+		}
 	glEnd();
 
 	//glFlush();
@@ -136,60 +137,64 @@ void reshape (int w, int h){ // function called when window size is changed
 
 void mouse(int button, int state, int x, int y)
 {
-    switch(button)
-    {
-    case GLUT_LEFT_BUTTON:
+	switch(button)
+	{
+		case GLUT_LEFT_BUTTON:
 
-        if(state==GLUT_DOWN)
-        {
-            start.x = x; //x1
-            start.y = y; //y1    
-			
-			xminSelect = xmin + (double)x/(XSize)*(xmax - xmin);
-			yminSelect = ymin + (double)y/(YSize)*(ymax - ymin);
-
-            cout<<"Initial pos set: (" << start.x << " " << start.y << endl;                  
-        }
-        if(state==GLUT_UP)
-        {
-            finish.x = x; //x2
-            finish.y = y; //y2
-            cout<<"Final pos set: (" << finish.x << " " << finish.y << endl;  
-
-			//xmin = start.x<finish.x?start.x: finish.x;
-			//xmax = start.x+finish.x - xmin;
-
-			//ymin = start.y<finish.y?start.y: finish.y;
-			//ymax = start.y + finish.y - ymin;
-
-			xmaxSelect = xmin + (double)x/(XSize)*(xmax - xmin);
-			ymaxSelect = ymin + (double)y/(YSize)*(ymax - ymin);
-
-			// check the ratio of the width and height, reset if it is too big or too small
-			double ratio = (xmaxSelect - xminSelect)/(ymaxSelect - yminSelect);
-			if (ratio > 1)
-			{	
-				xmaxSelect = xminSelect + (ymaxSelect - yminSelect);
-			}
-			else if (ratio < 1)
+			if(state==GLUT_DOWN)
 			{
-				ymaxSelect = yminSelect + (xmaxSelect - xminSelect);
+				start.x = x; //x1
+				start.y = y; //y1    
+
+				xminSelect = xmin + (double)x/(XSize)*(xmax - xmin);
+				yminSelect = ymin + (double)y/(YSize)*(ymax - ymin);
+
+				cout<<"Initial pos set: (" << start.x << " " << start.y << endl;                  
 			}
+			if(state==GLUT_UP)
+			{
+				finish.x = x; //x2
+				finish.y = y; //y2
+				cout<<"Final pos set: (" << finish.x << " " << finish.y << endl;  
 
-			// update the region
-			xminOld = xmin; xmaxOld = xmax;
-			yminOld = ymin; ymaxOld = ymax;
+				// if finish is too close to start, return 
+				if (fabs(finish.x - start.x) < delta || fabs(finish.y - start.y) < delta)
+					break;
+	
+				//xmin = start.x<finish.x?start.x: finish.x;
+				//xmax = start.x+finish.x - xmin;
 
-			xmin = xminSelect; xmax = xmaxSelect;
-			ymin = yminSelect; ymax = ymaxSelect;
+				//ymin = start.y<finish.y?start.y: finish.y;
+				//ymax = start.y + finish.y - ymin;
 
-			// repaint
-			glutPostRedisplay();
+				xmaxSelect = xmin + (double)x/(XSize)*(xmax - xmin);
+				ymaxSelect = ymin + (double)y/(YSize)*(ymax - ymin);
 
-        }
-        break;  
-        //glutPostRedisplay();
-    }
+				// check the ratio of the width and height, reset if it is too big or too small
+				double ratio = (xmaxSelect - xminSelect)/(ymaxSelect - yminSelect);
+				if (ratio > 1)
+				{	
+					xmaxSelect = xminSelect + (ymaxSelect - yminSelect);
+				}
+				else if (ratio < 1)
+				{
+					ymaxSelect = yminSelect + (xmaxSelect - xminSelect);
+				}
+
+				// update the region
+				xminOld = xmin; xmaxOld = xmax;
+				yminOld = ymin; ymaxOld = ymax;
+
+				xmin = xminSelect; xmax = xmaxSelect;
+				ymin = yminSelect; ymax = ymaxSelect;
+
+				// repaint
+				glutPostRedisplay();
+
+			}
+			break;  
+			//glutPostRedisplay();
+	}
 }
 
 void motion( int x, int y )
@@ -202,8 +207,8 @@ void motion( int x, int y )
 	glVertex2f(finish.x, start.y);
 	glEnd();
 
-    finish.x = x;
-    finish.y = y;
+	finish.x = x;
+	finish.y = y;
 
 	glBegin(GL_QUADS);
 	glColor3fv(white);
@@ -213,7 +218,7 @@ void motion( int x, int y )
 	glVertex2f(finish.x, start.y);
 	glEnd();
 
-    //glutPostRedisplay();
+	//glutPostRedisplay();
 }
 
 
@@ -269,7 +274,7 @@ void specialKeys( int key, int x, int y ) {
 
 		xmin = xminOld; xmax = xmaxOld;
 		ymin = yminOld; ymax = ymaxOld;
-		
+
 		glutPostRedisplay();
 
 	}
@@ -292,9 +297,9 @@ void specialKeys( int key, int x, int y ) {
 int main(int argc, char* argv[]){
 
 
-//	ReadPhantom();
-//	cout << phantom[0][0][0] << " " << phantom[217][125][58] << " " << phantom[217][125][59] << endl;
-//	//  Initialize GLUT and process user parameters
+	//	ReadPhantom();
+	//	cout << phantom[0][0][0] << " " << phantom[217][125][58] << " " << phantom[217][125][59] << endl;
+	//	//  Initialize GLUT and process user parameters
 	glutInit(&argc,argv);
 
 	//  Request double buffered true color window with Z-buffer
@@ -314,8 +319,8 @@ int main(int argc, char* argv[]){
 
 
 	// Callback functions
-    glutMouseFunc(mouse);
-    glutMotionFunc(motion);
+	glutMouseFunc(mouse);
+	glutMotionFunc(motion);
 
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
